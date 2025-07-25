edc7e8a9a2aaca670f167a1144c026d07aaa2586
unknown <zevikn@gmail.com>
2025-07-25 21:47:48 +0300

    remove debug printouts

--- tests/utils/verifyEvent.js
+++ tests/utils/verifyEvent.js
@@ -2,23 +2,21 @@ import { expect } from '@playwright/test';
 
 export async function verifyRequest(request, type, videoTime, userId) {
     let requestPostData;
-
     try {
         requestPostData = JSON.parse(request.postData());
     } catch (err) {
         throw new Error('Failed to parse request POST data: ' + err.message);
     }
-
-    console.log('📤 requestPostData:', requestPostData);
     expect(requestPostData.type).toBe(type);
     expect(requestPostData.videoTime).toBeGreaterThanOrEqual(videoTime);
     expect(requestPostData.userId).toBe(userId);
     expect(requestPostData.timestamp).toBeDefined();
+    //console.log('📤 requestPostData:', requestPostData); //use for debug only
 }
 
 export async function verifyResponse(response, statusCode, okStatus) {
     const responseBody = await response.json();
-    console.log('responseBody:', responseBody);
     expect(response.status()).toBe(statusCode);
     expect(responseBody.ok).toBe(okStatus);
+    //console.log('responseBody:', responseBody); //use for debug only
 }
