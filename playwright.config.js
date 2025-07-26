bd1f1cc1dbb2d01f01bdd995c6bb13ff29bf2edc
unknown <zevikn@gmail.com>
2025-07-26 22:30:26 +0300

    remove Report summary in PR comment from yml file

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,6 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
-    ['json', { outputFile: 'report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
