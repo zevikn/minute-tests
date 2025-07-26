40655fd77e807d0a25dca13e03a412f3269bb40c
unknown <zevikn@gmail.com>
2025-07-26 22:03:59 +0300

    test report summary in PR comment add

--- playwright.config.js
+++ playwright.config.js
@@ -26,6 +26,7 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: 'playwright-report/.report.json' }]
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
